adc6766c8ded4997f5295659e1d4911c368a668d
Don Ky <don.d.ky@gmail.com>
2020-07-14 00:42:45 -0400

    Partial implementation of draw, peek

--- oop-deck-of-cards/deck.py
+++ oop-deck-of-cards/deck.py
@@ -2,29 +2,40 @@ import card
 
 
 class Deck:
+
     def __init__(self):
+        # Instance variable
         self.cards = []
+        # Range over the cards
         for suit in card.SUITS:
             for value in card.VALUES:
-                # Initialize a new card object from Card class
-                card_init = card.Card(suit, value)
+                new_card = (value, "of", suit)
                 # Append each card to deck
-                self.cards.append(card_init)
+                self.cards.append(new_card)
 
     def num_cards(self):
         return len(self.cards)
 
+    def return_deck(self):
+        print(self.cards)
+
     def shuffle(self):
         pass
 
-    def peek(self, deck):
-        self.deck = deck
-        print(self.deck)
+    # Pop from beginning of the list?
+    def peek(self):
+        return self.cards.pop(0)
 
+    # Pop but don't return anything?
     def draw(self):
-        pass
+        self.cards.pop(0)
 
 
 myDeck = Deck()
 print(myDeck.num_cards())
-print(myDeck.peek(Deck))
+# print(myDeck.return_deck())
+# print(myDeck.peek())
+
+print(myDeck.draw())
+# This returns 51 elements.
+print(myDeck.num_cards())
